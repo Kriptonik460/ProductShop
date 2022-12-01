d190feee8c08a5ca1ecf1b9229ef0e13a9af65f6
Арсений Макаров <makki09092004@hotmail.com>
2022-12-01 18:23:47 +0300

    Добавьте файлы проекта.

---
+++ ProductShop/Resource/DbConnect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductShop.Resource
+{
+   static class DbConnect
+    {
+        public static ProductShopEntities db = new ProductShopEntities();
+    
+
+        static DbConnect()
+        {
+            
+        }
+    }
+
+}
